954825805d7825ff079163e7826683f223c8dc4c
Fabio Luiz <f247079@dac.unicamp.br>
2022-12-27 20:50:30 -0300

    Termina operação 1

--- main.c
+++ main.c
@@ -2,19 +2,17 @@
 #include "primos.h"
 
 int main(){
-    int Operacao, Posicao, Primo;
+    int Operacao, Posicao;
     printf("Insira a operação desejada:\n");
     printf("Encontrar primo: (1)\n");
     printf("Encontrar posição: (2)\n");
-    scanf("%d\n", &Operacao);
+    scanf("%d", &Operacao);
 
     if(Operacao == 1){
         printf("Digite a posição do primo: ");
-        scanf("%d\n", &Posicao);
-        Primo = Procura_primo(Posicao);
+        scanf("%d", &Posicao);
+        Procura_primo(Posicao);
     }
-
-    printf("%d", Primo);
 
     return 0;
 }
