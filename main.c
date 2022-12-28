13fec8b7ef6cc341467b49838181db657deb7ebd
Fabio Luiz <f247079@dac.unicamp.br>
2022-12-27 21:54:58 -0300

    Adicionada proteção contra inputs errados

--- main.c
+++ main.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "primos.h"
 
 int main(){
-    int Operacao, Posicao, Primo;
-    printf("Insira a operação desejada:\n");
-    printf("Encontrar primo: (1)\n");
-    printf("Encontrar posição: (2)\n");
-    scanf("%d", &Operacao);
+    int Operacao, Posicao, Primo, Aux;
+    char Repete = 'S';
 
-    if(Operacao == 1){
-        printf("Digite a posição do primo: ");
-        scanf("%d", &Posicao);
-        Procura_primo(Posicao);
-    } else{
-        printf("Digite o primo que deseja procurar: ");
-        scanf("%d", &Primo);
-        Procura_posicao(Primo);
+    while((Repete == 'S') || (Repete == 's')){
+        system("clear");
+        printf("Insira a operação desejada:\n");
+        printf("Encontrar primo: (1)\n");
+        printf("Encontrar posição: (2)\n");
+        Aux = scanf("%d", &Operacao);
+        if(Aux){
+            if(Operacao == 1){
+                printf("Digite a posição do primo: ");
+                Aux = scanf("%d", &Posicao);
+                if(Aux)
+                    Procura_primo(Posicao);
+                else
+                    printf("Valor inválido.\n");
+            } else if(Operacao == 2){
+                printf("Digite o primo que deseja procurar: ");
+                Aux = scanf("%d", &Primo);
+                if(Aux)
+                    Procura_posicao(Primo);
+                else
+                    printf("Valor inválido.\n");
+            } else
+                printf("Valor inválido.\n");
+            printf("\nDeseja continuar?(S/N): ");
+            scanf(" %c", &Repete);
+        } else{
+            printf("Valor inválido.\n");
+            exit(0);
+            }
     }
-
     return 0;
 }
